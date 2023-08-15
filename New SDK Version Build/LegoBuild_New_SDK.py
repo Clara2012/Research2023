7c039e5627b9ee3fee1571e5e35b73ac0ba1dc8f
Clara2012 <122822309+Clara2012@users.noreply.github.com>
2023-08-15 12:30:30 -0400

    Add files via upload

--- New SDK Version Build/LegoBuild_New_SDK.py
+++ New SDK Version Build/LegoBuild_New_SDK.py
@@ -3,6 +3,8 @@ import depthai as dai
 from depthai_sdk.trigger_action import Trigger
 from depthai_sdk.trigger_action.actions import RecordAction
 from waiting import wait
+import time
+
 
 
 with OakCamera() as oak:
@@ -10,25 +12,36 @@ with OakCamera() as oak:
     left = oak.create_camera('left', resolution='800p', fps=20, encode='H265')
     right = oak.create_camera('right', resolution='800p', fps=20, encode='H265')
 
+    #enable reading the trigger
+    color.initialControl.setFrameSyncMode(dai.CameraControl.FrameSyncMode.INPUT)
+    left.initialControl.setFrameSyncMode(dai.CameraControl.FrameSyncMode.INPUT)
+    right.initialControl.setFrameSyncMode(dai.CameraControl.FrameSyncMode.INPUT)
+
+    # create pipeline so we can write explicitly to it:
+    pipeline = oak.build()
+
 #this is manually starting the pipeline, whick is done in the oak.record or oak.create, but we must do it explisitly to enable frame synch mode
-    pipeline = dai.Pipeline()
 
-    RGB = pipeline.create(dai.node.ColorCamera)
-    RGB.initialControl.setFrameSyncMode(dai.CameraControl.FrameSyncMode.INPUT)
-
-    with dai.Device(pipeline) as device:
+    # script to read if trigger is sent
+    """""
+    def trig():
+        device = dai.Device(pipeline)
         queues = {}
-        queues[RGB]= device.getOutputQueue(RGB)
-        wait(lambda: queues.has() == True)
-
-    # view trigger
-
+        oakQueue = device.getOutputQueue()
+        wait(lambda: oakQueue.has() == True)
+        return (time.time)
+        #return time or bool to synch with
+ """""
     # Synchronize & save all (encoded) streams
     oak.record([color.out.encoded, left.out.encoded, right.out.encoded], './', RecordType.VIDEO)
+
     # Show color stream
     oak.visualize([color.out.camera], scale=2/3, fps=True)
 
-    oak.start(blocking=True)
+    # trigger = DetectionTrigger(input=Fsynch input, sent = True,)
+
+    # oak.trigger_action([color.out.camera], trig, oak.start(blocking=True))
+
     # oak.start uploads the pipeline to the device
     # I cannot access them to see if queues name has for trigger
     # This may be a new node ive added??? Try checking that out
